a5ef6db7630b80cc19ea2215f27a5e5c63cb3fdf
Stjepan <stjepan.crncic@gmail.com>
2023-05-02 23:14:56 +0200

    created a core domain with service provider

---
+++ app/Domain/Core/Providers/CoreServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Domain\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
